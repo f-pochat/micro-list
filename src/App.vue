d8b47bddd3894390e31d47e0ed895b18d3d64340
f-pochat <federico.pochat@ing.austral.edu.ar>
2023-05-17 15:16:44 -0300

    First

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>List</div>
+    <div>
+        <a href="#"> Github</a>
+    </div>
+    <div>
+      <a href="/home">Go to Home</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({});
+</script>
